0004cc03afba62d05f4a5fab0ea1073bae92da5a
Fabricio Garcia <gael.fabricio@gmail.com>
2020-12-16 17:48:06 -0600

    🚀feat: Generate Article scaffold
    This scaffold replaces the previous Article model rollbacked in the
    last commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_12_16_232004) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "published_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
